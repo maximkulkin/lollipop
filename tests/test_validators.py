bb750e1dea53157432147730bb174eacde5f5971
Maxim Kulkin <maxim.kulkin@gmail.com>
2016-07-24 00:37:40 -0700

    Add validators

---
+++ tests/test_validators.py
@@ -0,0 +1,263 @@
+from pytest import raises
+from contextlib import contextmanager
+from zephyr.validators import Predicate, Range, Length, NoneOf, AnyOf, Regexp
+from zephyr.errors import ValidationError
+import re
+
+
+@contextmanager
+def not_raises(exception_type):
+    try:
+        yield
+    except exception_type as err:
+        raise AssertionError(
+            "Did raise exception {0} when it should not!".format(
+                repr(exception_type)
+            )
+        )
+    except Exception as err:
+        raise AssertionError(
+            "An unexpected exception {0} raised.".format(repr(err))
+        )
+
+
+class TestPredicate:
+    def test_matching_values(self):
+        with not_raises(ValidationError):
+            Predicate(lambda x: x in ['foo', 'bar', 'baz'], 'Invalid')('foo')
+
+        with not_raises(ValidationError):
+            Predicate(lambda x: x in ['foo', 'bar', 'baz'], 'Invalid')('bar')
+
+    def test_raising_ValidationError_if_predicate_returns_False(self):
+        message = 'Invalid data'
+        with raises(ValidationError) as exc_info:
+            Predicate(lambda x: x in ['foo', 'bar'], message)('baz')
+        assert exc_info.value.messages == message
+
+
+class TestRange:
+    def test_matching_min_value(self):
+        with not_raises(ValidationError):
+            Range(min=1)(1)
+
+        with not_raises(ValidationError):
+            Range(min=1)(2)
+
+    def test_raising_ValidationError_when_matching_min_value_and_given_value_is_less(self):
+        with raises(ValidationError) as exc_info:
+            Range(min=1)(0)
+        assert exc_info.value.messages == \
+            Range.default_error_messages['min'].format(min=1)
+
+    def test_customzing_min_error_message(self):
+        message = 'Value {data} should be at least {min}'
+        with raises(ValidationError) as exc_info:
+            Range(min=1, error_messages={'min': message})(0)
+        assert exc_info.value.messages == message.format(data=0, min=1)
+
+    def test_matching_max_value(self):
+        with not_raises(ValidationError):
+            Range(max=1)(1)
+
+        with not_raises(ValidationError):
+            Range(max=1)(0)
+
+    def test_raising_ValidationError_when_matching_max_value_and_given_value_is_greater(self):
+        with raises(ValidationError) as exc_info:
+            Range(max=1)(2)
+        assert exc_info.value.messages == \
+            Range.default_error_messages['max'].format(max=1)
+
+    def test_customzing_max_error_message(self):
+        message = 'Value {data} should be at most {max}'
+        with raises(ValidationError) as exc_info:
+            Range(max=1, error_messages={'max': message})(2)
+        assert exc_info.value.messages == message.format(data=2, max=1)
+
+    def test_matching_range(self):
+        with not_raises(ValidationError):
+            Range(min=1, max=5)(1)
+
+        with not_raises(ValidationError):
+            Range(min=1, max=5)(3)
+
+        with not_raises(ValidationError):
+            Range(min=1, max=5)(5)
+
+    def test_raising_ValidationError_when_matching_range_and_given_value_is_less(self):
+        with raises(ValidationError) as exc_info:
+            Range(min=1, max=5)(0)
+        assert exc_info.value.messages == \
+            Range.default_error_messages['range'].format(min=1, max=5)
+
+    def test_raising_ValidationError_when_matching_range_and_given_value_is_greater(self):
+        with raises(ValidationError) as exc_info:
+            Range(min=1, max=5)(6)
+        assert exc_info.value.messages == \
+            Range.default_error_messages['range'].format(min=1, max=5)
+
+    def test_customzing_range_error_message(self):
+        message = 'Value {data} should be between {min} and {max}'
+        with raises(ValidationError) as exc_info:
+            Range(min=1, max=5, error_messages={'range': message})(0)
+        assert exc_info.value.messages == message.format(data=0, min=1, max=5)
+
+
+class TestLength:
+    def test_matching_exact_value(self):
+        with not_raises(ValidationError):
+            Length(exact=2)([1, 2])
+
+    def test_raising_ValidationError_when_matching_exact_value_and_given_value_does_not_match(self):
+        with raises(ValidationError) as exc_info:
+            Length(exact=3)([1, 2])
+        assert exc_info.value.messages == \
+            Length.default_error_messages['exact'].format(exact=3)
+
+    def test_customizing_exact_error_message(self):
+        message = 'Value {data} length is not {exact}'
+        with raises(ValidationError) as exc_info:
+            Length(exact=3, error_messages={'exact': message})([1, 2])
+        assert exc_info.value.messages == message.format(data=[1, 2], exact=3)
+
+    def test_matching_min_value(self):
+        with not_raises(ValidationError):
+            Length(min=1)([1])
+
+        with not_raises(ValidationError):
+            Length(min=1)([1, 2])
+
+    def test_raising_ValidationError_when_matching_min_value_and_given_value_is_less(self):
+        with raises(ValidationError) as exc_info:
+            Length(min=1)([])
+        assert exc_info.value.messages == \
+            Length.default_error_messages['min'].format(min=1)
+
+    def test_customzing_min_error_message(self):
+        message = 'Length {length} of {data} should be at least {min}'
+        with raises(ValidationError) as exc_info:
+            Length(min=1, error_messages={'min': message})([])
+        assert exc_info.value.messages == message.format(data=[], length=0, min=1)
+
+    def test_matching_max_value(self):
+        with not_raises(ValidationError):
+            Length(max=1)([1])
+
+        with not_raises(ValidationError):
+            Length(max=1)([])
+
+    def test_raising_ValidationError_when_matching_max_value_and_given_value_is_greater(self):
+        with raises(ValidationError) as exc_info:
+            Length(max=1)([1, 2])
+        assert exc_info.value.messages == \
+            Length.default_error_messages['max'].format(max=1)
+
+    def test_customzing_max_error_message(self):
+        message = 'Length {length} of {data} should be at most {max}'
+        with raises(ValidationError) as exc_info:
+            Length(max=1, error_messages={'max': message})([1, 2])
+        assert exc_info.value.messages == \
+            message.format(data=[1, 2], length=2, max=1)
+
+    def test_matching_range(self):
+        with not_raises(ValidationError):
+            Length(min=1, max=5)([1])
+
+        with not_raises(ValidationError):
+            Length(min=1, max=5)([1, 2, 3])
+
+        with not_raises(ValidationError):
+            Length(min=1, max=5)([1, 2, 3, 4, 5])
+
+    def test_raising_ValidationError_when_matching_range_and_given_value_is_less(self):
+        with raises(ValidationError) as exc_info:
+            Length(min=1, max=5)([])
+        assert exc_info.value.messages == \
+            Length.default_error_messages['range'].format(min=1, max=5)
+
+    def test_raising_ValidationError_when_matching_range_and_given_value_is_greater(self):
+        with raises(ValidationError) as exc_info:
+            Length(min=1, max=5)([1, 2, 3, 4, 5, 6])
+        assert exc_info.value.messages == \
+            Length.default_error_messages['range'].format(min=1, max=5)
+
+    def test_customzing_range_error_message(self):
+        message = 'Length {length} of {data} should be between {min} and {max}'
+        with raises(ValidationError) as exc_info:
+            Length(min=1, max=5, error_messages={'range': message})([])
+        assert exc_info.value.messages == \
+            message.format(data=[], length=0, min=1, max=5)
+
+
+class TestNoneOf:
+    def test_matching_values_other_than_given_values(self):
+        with not_raises(ValidationError):
+            NoneOf(['foo', 'bar'])('baz')
+
+    def test_raising_ValidationError_when_value_is_one_of_forbidden_values(self):
+        with raises(ValidationError) as exc_info:
+            NoneOf(['foo', 'bar'])('foo')
+        assert exc_info.value.messages == NoneOf.default_error_messages['invalid']
+
+    def test_customizing_error_message(self):
+        message = 'Value {data} in {values}'
+        with raises(ValidationError) as exc_info:
+            NoneOf(['foo', 'bar'], error=message)('foo')
+        assert exc_info.value.messages == message.format(data='foo',
+                                                         values=['foo', 'bar'])
+
+
+class TestAnyOf:
+    def test_matching_given_values(self):
+        with not_raises(ValidationError):
+            AnyOf(['foo', 'bar'])('foo')
+
+        with not_raises(ValidationError):
+            AnyOf(['foo', 'bar'])('bar')
+
+    def test_raising_ValidationError_when_value_is_other_than_given_values(self):
+        with raises(ValidationError) as exc_info:
+            AnyOf(['foo', 'bar'])('baz')
+        assert exc_info.value.messages == AnyOf.default_error_messages['invalid']
+
+    def test_customizing_error_message(self):
+        message = 'Value {data} not in {choices}'
+        with raises(ValidationError) as exc_info:
+            AnyOf(['foo', 'bar'], error=message)('baz')
+        assert exc_info.value.messages == message.format(data='baz',
+                                                         choices=['foo', 'bar'])
+
+
+class TestRegexp:
+    def test_matching_by_string_regexp(self):
+        with not_raises(ValidationError):
+            Regexp('a+b')('aaab')
+
+    def test_matching_by_string_regexp_with_flags(self):
+        with not_raises(ValidationError):
+            Regexp('a+b', re.IGNORECASE)('AaAB')
+
+    def test_matching_by_regexp(self):
+        with not_raises(ValidationError):
+            Regexp(re.compile('a+b'))('aab')
+
+    def test_matching_by_regexp_ignores_flags(self):
+        with raises(ValidationError):
+            Regexp(re.compile('a+b'), flags=re.IGNORECASE)('AAB')
+
+    def test_raising_ValidationError_if_given_string_does_not_match_string_regexp(self):
+        with raises(ValidationError) as exc_info:
+            Regexp('a+b')('bbc')
+        assert exc_info.value.messages == Regexp.default_error_messages['invalid']
+
+    def test_raising_ValidationError_if_given_string_does_not_match_regexp(self):
+        with raises(ValidationError) as exc_info:
+            Regexp(re.compile('a+b'))('bbc')
+        assert exc_info.value.messages == Regexp.default_error_messages['invalid']
+
+    def test_customizing_error_message(self):
+        message = 'Value {data} does not match {regexp}'
+        with raises(ValidationError) as exc_info:
+            Regexp('a+b', error=message)('bbc')
+        assert exc_info.value.messages == message.format(data='bbc', regexp='a+b')
---
+++ zephyr/validators.py
@@ -0,0 +1,203 @@
+from zephyr.errors import ValidationError, ErrorMessagesMixin
+from zephyr.compat import string_types
+import re
+
+
+class Validator(ErrorMessagesMixin, object):
+    pass
+
+
+class Predicate(Validator):
+    def __init__(self, predicate, error='Invalid data'):
+        super(Predicate, self).__init__()
+        self.predicate = predicate
+        self.error = error
+
+    def __call__(self, value):
+        if not self.predicate(value):
+            raise ValidationError(self.error)
+
+    def __repr__(self):
+        return '<{klass} predicate={predicate} error={error}>'.format(
+            klass=self.__class__.__name__,
+            predicate=self.predicate, error=self.error,
+        )
+
+
+class Range(Validator):
+    default_error_messages = {
+        'min': 'Value should be at least {min}',
+        'max': 'Value should be at most {max}',
+        'range': 'Value should be at least {min} and at most {max}',
+    }
+
+    def __init__(self, min=None, max=None, **kwargs):
+        super(Range, self).__init__(**kwargs)
+        self.min = min
+        self.max = max
+
+    def _fail(self, key, **kwargs):
+        super(Range, self)._fail(key, min=self.min, max=self.max, **kwargs)
+
+    def __call__(self, value):
+        if self.min is not None and self.max is not None:
+            if value < self.min or value > self.max:
+                self._fail('range', data=value)
+        elif self.min is not None:
+            if value < self.min:
+                self._fail('min', data=value)
+        elif self.max is not None:
+            if value > self.max:
+                self._fail('max', data=value)
+
+    def __repr__(self):
+        return '<{klass} {properties}>'.format(
+            klass= self.__class__.__name__,
+            properties=' '.join(['%s=%s' % (k, repr(v))
+                                 for k, v in iteritems({'min': self.min,
+                                                        'max': self.max})
+                                 if v is not None])
+        )
+
+
+class Length(Validator):
+    """Validator that checks value length (using ``len()``) to be in given range.
+
+    :param int exact: Exact length. If provided, ``min`` and ``max`` are not checked.
+        If not provided, ``min`` and ``max`` checks are performed.
+    :param int min: Minimum length. If not provided, minimum length won't be checked.
+    :param int max: Maximum length. If not provided, maximum length won't be checked.
+    :param str error: Error message in case of validation error.
+        Can be interpolated with ``data``, ``length``, ``exact``, ``min`` or ``max``.
+    """
+    default_error_messages = {
+        'exact': 'Length should be {exact}',
+        'min': 'Length should be at least {min}',
+        'max': 'Length should be at most {max}',
+        'range': 'Length should be at least {min} and at most {max}',
+    }
+
+    def __init__(self, exact=None, min=None, max=None, **kwargs):
+        super(Length, self).__init__(**kwargs)
+        self.exact = exact
+        self.min = min
+        self.max = max
+
+    def _fail(self, key, **kwargs):
+        super(Length, self)._fail(key, exact=self.exact, min=self.min, max=self.max,
+                                  **kwargs)
+
+    def __call__(self, value):
+        length = len(value)
+        if self.exact is not None:
+            if length != self.exact:
+                self._fail('exact', data=value, length=length)
+        elif self.min is not None and self.max is not None:
+            if length < self.min or length > self.max:
+                self._fail('range', data=value, length=length)
+        elif self.min is not None:
+            if length < self.min:
+                self._fail('min', data=value, length=length)
+        elif self.max is not None:
+            if length > self.max:
+                self._fail('max', data=value, length=length)
+
+    def __repr__(self):
+        if self.exact is not None:
+            return '<{klass} exact={exact}>'.format(
+                klass=self.__class__.__name__,
+                exact=self.exact
+            )
+        else:
+            super(Length, self).__repr__()
+
+
+class NoneOf(Validator):
+    """Validator that succeeds if ``value`` is not a member of given ``values``.
+
+    :param iterable values: A sequence of invalid values.
+    :param str error: Error message in case of validation error.
+        Can be interpolated with ``data`` and ``values``.
+    """
+
+    default_error_messages = {
+        'invalid': 'Invalid data',
+    }
+
+    def __init__(self, values, error=None, **kwargs):
+        super(NoneOf, self).__init__(**kwargs)
+        self.values = values
+        if error is not None:
+            self._error_messages['invalid'] = error
+
+    def __call__(self, value):
+        if value in self.values:
+            self._fail('invalid', data=value, values=self.values)
+
+    def __repr__(self):
+        return '<{klass} {values}>'.format(
+            klass=self.__class__.__name__,
+            values=repr(self.values),
+        )
+
+
+class AnyOf(Validator):
+    """Validator that succeeds if ``value`` is a member of given ``choices``.
+
+    :param iterable choices: A sequence of allowed values.
+    :param str error: Error message in case of validation error.
+        Can be interpolated with ``data`` and ``choices``.
+    """
+
+    default_error_messages = {
+        'invalid': 'Invalid choice',
+    }
+
+    def __init__(self, choices, error=None, **kwargs):
+        super(AnyOf, self).__init__(**kwargs)
+        self.choices = choices
+        if error is not None:
+            self._error_messages['invalid'] = error
+
+    def __call__(self, value):
+        if value not in self.choices:
+            self._fail('invalid', data=value, choices=self.choices)
+
+    def __repr__(self):
+        return '<{klass} {choices}>'.format(
+            klass=self.__class__.__name__,
+            choices=repr(self.choices),
+        )
+
+
+class Regexp(Validator):
+    """Validator that succeeds if ``value`` matches given ``regex``.
+
+    :param str regexp: Regular expression string.
+    :param int flags: Regular expression flags, e.g. re.IGNORECASE.
+        Not used if regexp is not a string.
+    :param str error: Error message in case of validation error.
+        Can be interpolated with ``data`` and ``regexp``.
+    """
+
+    default_error_messages = {
+        'invalid': 'String does not match expected pattern',
+    }
+
+    def __init__(self, regexp, flags=0, error=None, **kwargs):
+        super(Regexp, self).__init__(**kwargs)
+        if isinstance(regexp, string_types):
+            regexp = re.compile(regexp, flags)
+        self.regexp = regexp
+        if error is not None:
+            self._error_messages['invalid'] = error
+
+    def __call__(self, value):
+        if self.regexp.match(value) is None:
+            self._fail('invalid', data=value, regexp=self.regexp.pattern)
+
+    def __repr__(self):
+        return '<{klass} {regexp}>'.format(
+            klass=self.__class__.__name__,
+            regexp=self.regexp.pattern,
+        )
