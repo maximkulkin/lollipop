a4e130091b300736575d88c0282c2239fd5087a4
Maxim Kulkin <maxim.kulkin@gmail.com>
2016-07-27 13:19:37 -0700

    Add more detailed tests for Predicate validator

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -24,12 +24,17 @@ def not_raises(exception_type):
 class TestPredicate:
     def test_matching_values(self):
         with not_raises(ValidationError):
-            Predicate(lambda x: x in ['foo', 'bar', 'baz'], 'Invalid')('foo')
+            Predicate(lambda x: x in ['foo', 'bar', 'baz'])('foo')
 
         with not_raises(ValidationError):
-            Predicate(lambda x: x in ['foo', 'bar', 'baz'], 'Invalid')('bar')
+            Predicate(lambda x: x in ['foo', 'bar', 'baz'])('bar')
 
     def test_raising_ValidationError_if_predicate_returns_False(self):
+        with raises(ValidationError) as exc_info:
+            Predicate(lambda x: x in ['foo', 'bar'])('baz')
+        assert exc_info.value.messages == Predicate.default_error_messages['invalid']
+
+    def test_customizing_validation_error(self):
         message = 'Invalid data'
         with raises(ValidationError) as exc_info:
             Predicate(lambda x: x in ['foo', 'bar'], message)('baz')
